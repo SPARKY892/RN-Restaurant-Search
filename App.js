d21b83c98c8b462799e366b212fadfd22855f38a
Mark <marksmellie892@gmail.com>
2021-02-10 17:04:27 +0000

    Created results show screen

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import ResultsShowScreen from './src/screens/ResultsShowScreen';
 import SearchScreen from './src/screens/SearchScreen';
 
 const navigator = createStackNavigator({
-	Search: SearchScreen
+	Search: SearchScreen,
+	ResultsShow: ResultsShowScreen
 }, {
 	initialRouteName: 'Search',
 	defaultNavigationOptions: {
-		title: 'Restaurant Search'
+		title: 'Business Search'
 	}
 });
 
